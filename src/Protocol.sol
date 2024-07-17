21c0d474b8a6d767749888bf84d5578b7141de5c
PhantomOz <faniogor@gmail.com>
2024-07-17 02:12:39 +0100

    This adds state to the contract

--- src/Protocol.sol
+++ src/Protocol.sol
@@ -1,4 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Protocol {}
+contract Protocol {
+    mapping(address => address[]) private s_ownerToContracts; //maps an owner to the contracts they have created.
+    mapping(address => ContractDetail) private s_contractToDetail; //maps a contract to its details.
+
+    enum ContractType {
+        erc2o,
+        erc721,
+        erc1155,
+        governance,
+        custom
+    }
+
+    struct ContractDetail {
+        address _creator;
+        string _name;
+        bytes32 _abi;
+        ContractType _type;
+        bytes32 _sourceCode;
+        uint256 _createdAt;
+    }
+}
